8216165574b30f80255b4ae30f3d433a51740a72
mateofrito <46681151+mateofrito@users.noreply.github.com>
2019-03-19 14:54:26 -0400

    intializer complete

---
+++ src/test/java/io/mattfry/gamecollector/GamecollectorApplicationTests.java
@@ -0,0 +1,16 @@
+package io.mattfry.gamecollector;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GamecollectorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
