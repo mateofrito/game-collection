ec98698db6c28d871df31862f17958869f7e84b7
mateofrito <46681151+mateofrito@users.noreply.github.com>
2019-03-25 22:37:36 -0400

    playing more with modals and drop downs

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -11,15 +11,61 @@ function main() {
   })
 
   events.on(getAppContext(), 'click', () => {
-    if (event.target.classList.contains('add-console__submit')) {
-      const consoleName = document.querySelector('.add-console__full-name').value
-      const shortName = document.querySelector('.add-console__short-name').value
-       
-        api.postRequest('/consoles/add', {
-        consoleName: consoleName,
-        shortName: shortName
-      }, (gameconsoles) => getAppContext().innerHTML = GameConsoles(gameconsoles))
-    }
+          //console submit
+          if (event.target.classList.contains('add-console__submit')) {
+            const consoleName = document.querySelector('.add-console__full-name').value
+            const shortName = document.querySelector('.add-console__short-name').value
+            
+              api.postRequest('/consoles/add', {
+              consoleName: consoleName,
+              shortName: shortName
+            }, (gameconsoles) => getAppContext().innerHTML = GameConsoles(gameconsoles))
+          }
+
+          //add console modal
+          if (event.target.classList.contains('console-trigger')){
+          var consolemodal = document.querySelector(".modal-console");
+          var consoletrigger = document.querySelector(".console-trigger")
+          var closeButton = document.querySelector(".close-button");
+          
+          function toggleConsoleModal() {
+            consolemodal.classList.toggle("show-consolemodal");
+          }
+          
+          function windowOnClick(event) {
+            if (event.target === consolemodal) {
+                toggleConsoleModal();
+            }
+          }
+          consoletrigger.addEventListener("click", toggleConsoleModal);
+          closeButton.addEventListener("click", toggleConsoleModal);
+          window.addEventListener("click", windowOnClick);
+
+        }
+
+        //add game modal
+        if (event.target.classList.contains('game-trigger')){
+          var gameemodal = document.querySelector(".modal-gamee");
+          var gametrigger = document.querySelector(".game-trigger")
+          var closeButton2 = document.querySelector(".close-button2");
+          
+          function toggleGameModal() {
+            gamemodal.classList.toggle("show-gamemodal");
+          }
+          
+          function windowOnClick(event) {
+            if (event.target === gamemodal) {
+                toggleGameModal();
+            }
+          }
+          gametrigger.addEventListener("click", toggleGameModal);
+          closeButton2.addEventListener("click", toggleGameModal);
+          window.addEventListener("click", windowOnClick);
+
+        }
+
+
+
   })
 
  
@@ -28,23 +74,6 @@ function getAppContext() {
   return document.querySelector("#app")
 }
   
-        // var consolemodal = document.querySelector(".modal-console");
-        // var consoletrigger = document.querySelector(".console-trigger")
-        // var closeButton = document.querySelector(".close-button");
-
-        // function toggleConsoleModal() {
-        //   consolemodal.classList.toggle("show-consolemodal");
-        // }
-
-        // function windowOnClick(event) {
-        //   if (event.target === consolemodal) {
-        //       toggleConsoleModal();
-        //   }
-        // }
-
-        // consoletrigger.addEventListener("click", toggleConsoleModal);
-        // closeButton.addEventListener("click", toggleConsoleModal);
-        // window.addEventListener("click", windowOnClick);
 
       
 
--- src/main/resources/static/js/components/GameConsoles.js
+++ src/main/resources/static/js/components/GameConsoles.js
@@ -2,6 +2,27 @@ import Games from './Games'
 
 export default function GameConsoles(gameconsoles) {
     return `
+    <div class="buttons">
+        <button class="console-trigger">Add Console</button>
+        <button class="game-trigger">Add Game</button>
+    </div>
+    <div class ="modal-console">
+        <div class="consolemodal-content">
+                <span class="close-button">X</span>
+                <input type="text" class="add-console__full-name" placeholder="Console Name">
+                <input type="text" class="add-console__short-name" placeholder="Console Short Name">
+                <button class="add-console__submit">Add Console</button>
+        </div>
+   </div>
+   <div class ="modal-game">
+               <div class="gamemodal-content">
+                       <span class="close-button2">X</span>
+                         <input type="text" class="add-game__name" placeholder="Name">
+                         <input type="text" class="add-game__yearreleased" placeholder="Year Released">
+                     
+                         <button class="add-game__submit">Add Game</button>
+                 </div>
+   </div>
       <ul class="consoles">
         ${gameconsoles.map(gameconsole => {
             return `
@@ -52,11 +73,18 @@ export default function GameConsoles(gameconsoles) {
         //         </div>
         //   </div>
         //   <div class ="modal-game">
-        //         <div class="gamemodal-content">
-        //                 <span class="close-button">X</span>
-        //                 <input type="text" class="add-game__name" placeholder="Name">
-        //                 <input type="text" class="add-game__yearreleased" placeholder="Year Released">
-        //                 <select class="add-game__console" placeholder="Console">${gameconsole.consoleName}</select>
-        //                 <button class="add-game__submit">Add Game</button>
-        //         </div>
-        //   </div>
+        //        <div class="gamemodal-content">
+        //                <span class="close-button">X</span>
+        //                  <input type="text" class="add-game__name" placeholder="Name">
+        //                  <input type="text" class="add-game__yearreleased" placeholder="Year Released">
+        //                  <select class="add-game__console" placeholder="Console">${gameconsole.consoleName}</select>
+        //                  <button class="add-game__submit">Add Game</button>
+        //          </div>
+        //    </div>
+
+    //     <!--  <select class="add-game__console" placeholder="Console">
+    //     <option value="0">Select Console</option>
+    //     <option value="${gameconsole.consoleName}"
+    //             text="${gameconsole.consoleName}">
+    //     </option>
+    //  </select> --!>
