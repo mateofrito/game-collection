03ef9cff4c7db8c7ad5346621d1fbeb97e49958f
mateofrito <46681151+mateofrito@users.noreply.github.com>
2019-03-27 21:52:09 -0400

    close buttons on modals are working added accordions

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -26,45 +26,103 @@ function main() {
 
           //add console modal
           if (event.target.classList.contains('console-trigger')){
-          var consolemodal = document.querySelector(".modal-console");
-          var consoletrigger = document.querySelector(".console-trigger")
-          var closeButton = document.querySelector(".close-button");
+                var consolemodal = document.querySelector(".modal-console");
+                var consoletrigger = document.querySelector(".console-trigger")
+                var closeButton = document.querySelector(".close-button");
+
+                function toggleConsoleModal() {
+                  consolemodal.classList.toggle("show-consolemodal");
+                }
+                
+                function windowOnConsoleClick(event) {
+                        if (event.target === consolemodal) {
+                          toggleConsoleModal();
+                        }
+                }
+
+
+                consoletrigger.addEventListener("click", toggleConsoleModal);
+                closeButton.addEventListener("click", toggleConsoleModal);
+                window.addEventListener("click", windowOnConsoleClick);
+
+
+}
+          //close button
+          if (event.target.classList.contains('close-button')){
+              var closeButton = document.querySelector(".close-button");
+              var consolemodal = document.querySelector(".modal-console");
+
+              function closeTheGoddamnModal(){
+                consolemodal.classList.toggle("show-consolemodal");
+
+              }
+
+              closeButton.addEventListener("click", closeTheGoddamnModal);
+
+
+          }
+           
+             
+            
+            
+        //add game modal
+        if (event.target.classList.contains('game-trigger')){
+          var gamemodal = document.querySelector(".modal-game");
+          var gametrigger = document.querySelector(".game-trigger")
+          var closeButton2 = document.querySelector(".close-button2");
           
-          function toggleConsoleModal() {
-            consolemodal.classList.toggle("show-consolemodal");
+          function toggleGameModal() {
+            gamemodal.classList.toggle("show-gamemodal");
           }
           
-          function windowOnConsoleClick(event) {
-            if (event.target === consolemodal) {
-                toggleConsoleModal();
+          function windowOnGameClick(event) {
+            if (event.target === gamemodal) {
+                toggleGameModal();
             }
           }
-          consoletrigger.addEventListener("click", toggleConsoleModal);
-          closeButton.addEventListener("click", toggleConsoleModal);
-          window.addEventListener("click", windowOnConsoleClick);
+          gametrigger.addEventListener("click", toggleGameModal);
+          closeButton2.addEventListener("click", toggleGameModal);
+          window.addEventListener("click", windowOnGameClick);
 
         }
 
-        //add game modal
-        // if (event.target.classList.contains('game-trigger')){
-        //   var gamemodal = document.querySelector(".modal-game");
-        //   var gametrigger = document.querySelector(".game-trigger")
-        //   var closeButton2 = document.querySelector(".close-button2");
-          
-        //   function toggleGameModal() {
-        //     gamemodal.classList.toggle("show-gamemodal");
-        //   }
-          
-        //   function windowOnGameClick(event) {
-        //     if (event.target === gamemodal) {
-        //         toggleGameModal();
-        //     }
-        //   }
-        //   gametrigger.addEventListener("click", toggleGameModal);
-        //   closeButton2.addEventListener("click", toggleGameModal);
-        //   window.addEventListener("click", windowOnGameClick);
+        //close button
+        if (event.target.classList.contains('close-button2')){
+          var closeButton = document.querySelector(".close-button2");
+          var gamemodal = document.querySelector(".modal-game");
 
-        // }
+          function closeTheGoddamnModal(){
+            gamemodal.classList.toggle("show-gamemodal");
+
+          }
+
+          closeButton.addEventListener("click", closeTheGoddamnModal);
+
+
+      }
+      //console accordions
+      if (event.target.classList.contains('console__header')){
+          var accordion = document.getElementsByClassName("console__header");
+          var i;
+                for (i = 0; i < accordion.length; i++) {
+                  accordion[i].addEventListener("click", function() {
+                    this.classList.toggle("active");
+                    var panel = this.nextElementSibling;
+                    if (panel.style.display === "block") {
+                      panel.style.display = "none";
+                    } else {
+                      panel.style.display = "block";
+                    }
+                  });
+                }
+           
+
+
+
+
+
+
+      }
 
 
 
@@ -75,7 +133,28 @@ function main() {
 function getAppContext() {
   return document.querySelector("#app")
 }
+// if (event.target.classList.contains('console-trigger')){
+//   var consolemodal = document.querySelector(".modal-console");
+//   var consoletrigger = document.querySelector(".console-trigger")
+//   var closeButton = document.querySelector(".close-button");
+
+//   function toggleConsoleModal() {
+//     consolemodal.classList.toggle("show-consolemodal");
+//   }
   
+//   function windowOnConsoleClick(event) {
+//           if (event.target === consolemodal) {
+//             toggleConsoleModal();
+//           }
+//   }
+
+
+//   consoletrigger.addEventListener("click", toggleConsoleModal);
+//   closeButton.addEventListener("click", toggleConsoleModal);
+//   window.addEventListener("click", windowOnConsoleClick);
+
+
+// }
 
       
 
