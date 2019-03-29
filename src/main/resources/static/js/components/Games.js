96e601d05125bd17e77c3d7f79c03160f9024197
mateofrito <46681151+mateofrito@users.noreply.github.com>
2019-03-29 19:07:53 -0400

    not working

--- src/main/resources/static/js/components/Games.js
+++ src/main/resources/static/js/components/Games.js
@@ -5,6 +5,7 @@ export default function Games(games) {
                     <img class="game__imagepath" src="${game.imagePath}"></img>
                     <p><h3 class="game__title">${game.gameTitle}</h3>
                     <h5 class="game__yearreleased">${game.yearReleased}</h5></p>
+                    
                 </li>
             `;
         }).join('')
